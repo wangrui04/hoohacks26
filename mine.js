ae0ced6067cf6ce855a763f2ddfff4cf5c55c279
zachkr05 <zachkr05@gmail.com>
2026-03-21 13:50:31 -0400

    Mines done

---
+++ mine.js
@@ -0,0 +1,11 @@
+
+
+export class Mine {
+  constructor(x, y, { reward = 10, level = 1, risk = 0.5 } = {}) {
+    this.x = x;
+    this.y = y;
+    this.reward = reward;
+    this.level = level;
+    this.risk = risk;
+  }
+}
---
+++ player.js
@@ -0,0 +1,24 @@
+
+
+
+// --- Player ---
+export class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.curr_money = 0;
+    this.owned_mines = [];
+    this.owned_rivers = [];
+    this.previous_actions = []; //should be a tuple of action and timestep
+  }
+
+  /** Log an action to history. */
+  recordAction(action) {
+    this.previous_actions.push({ action, timestamp: Date.now() });
+  }
+
+  /** Check if the player has reached the win condition. */
+  hasWon() {
+    return this.curr_money >= WIN_GOLD;
+  }
+}
